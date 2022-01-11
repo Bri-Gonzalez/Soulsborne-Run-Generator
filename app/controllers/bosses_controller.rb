f39bda3d0c8d119ee2b39d9a757f549093215808
Briana Gonzalez <brianarachelgonzalez@gmail.com>
2022-01-11 12:24:41 -0600

    updated game routes, controller, and model

--- app/controllers/bosses_controller.rb
+++ app/controllers/bosses_controller.rb
@@ -1,5 +1,5 @@
 class BossesController < ApplicationController
-  before_action :set_boss, only: [:show, :update, :destroy]
+  before_action :set_boss, only: [:show]
 
   # GET /bosses
   def index
@@ -13,39 +13,10 @@ class BossesController < ApplicationController
     render json: @boss
   end
 
-  # POST /bosses
-  def create
-    @boss = Boss.new(boss_params)
-
-    if @boss.save
-      render json: @boss, status: :created, location: @boss
-    else
-      render json: @boss.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /bosses/1
-  def update
-    if @boss.update(boss_params)
-      render json: @boss
-    else
-      render json: @boss.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /bosses/1
-  def destroy
-    @boss.destroy
-  end
-
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_boss
-      @boss = Boss.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def boss_params
-      params.require(:boss).permit(:name, :game_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_boss
+    @boss = Boss.find(params[:id])
+  end
 end
