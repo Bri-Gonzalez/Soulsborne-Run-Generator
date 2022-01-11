c3dd51678ff0477220183432e8c12b176dc01bd5
Briana Gonzalez <brianarachelgonzalez@gmail.com>
2022-01-11 12:31:06 -0600

    added scaffold for weapon

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,4 @@
 class Game < ApplicationRecord
   has_many :bosses, dependent: :destroy
+  has_many :weapons, dependent: :destroy
 end
---
+++ db/migrate/20220111183000_create_weapons.rb
@@ -0,0 +1,10 @@
+class CreateWeapons < ActiveRecord::Migration[6.1]
+  def change
+    create_table :weapons do |t|
+      t.string :name
+      t.references :game, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
